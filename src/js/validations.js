699203347990098c41b7cf573f59dbbfe6e4497d
Alan Sanchez <sanchezpineda03@gmail.com>
2017-04-01 11:50:46 -0600

    #2. Fixed minor validation and error styles.

--- src/js/validations.js
+++ src/js/validations.js
@@ -2,6 +2,11 @@
   $('#contact-form').validate({
     errorPlacement: function (error, element) {
       element.closest('.input-wrapper').append(error);
+    },
+    rules: {
+      phone: {
+        number: true
+      }
     }
   });
 })();
