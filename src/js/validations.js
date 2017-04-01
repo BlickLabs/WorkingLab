be8769308e3cfbee3571752479781e0557c0fe22
Alan Sanchez <sanchezpineda03@gmail.com>
2017-04-01 12:03:35 -0600

    #2. Added validation to homepage newsletter form.

--- src/js/validations.js
+++ src/js/validations.js
@@ -1,4 +1,5 @@
 (function () {
+  $('#homepage-newsletter').validate();
   $('#contact-form').validate({
     errorPlacement: function (error, element) {
       element.closest('.input-wrapper').append(error);
