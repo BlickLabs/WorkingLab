41c047ba64a0689e489d5a9a71bcec4a2326dbed
Alan Sanchez <sanchezpineda03@gmail.com>
2017-04-08 18:15:28 -0500

    Fixed newsletter message.

--- src/js/app.js
+++ src/js/app.js
@@ -11,9 +11,10 @@
         fields = $(form).find('select, input, textarea, button').not('[disabled]'),
         formMessage = $(form).find('.form-message'),
         successMessage = $('<i class="fa fa-check-circle"></i><span>Mensaje enviado exitosamente</span>'),
+        newsletterMessage = $('<i class="fa fa-check-circle"></i><span>Suscripción realizada con éxito</span>'),
         errorMessage = $('<i class="fa fa-times-circle"></i><span>Ocurrió un error</span>'),
-        setMessage = function (success) {
-          var message = success ? successMessage : errorMessage;
+        setMessage = function (success, newsletter) {
+          var message = success ? (newsletter ? newsletterMessage : successMessage) : errorMessage;
           fields.removeAttr('disabled');
           formMessage.removeClass(success ? 'error' : 'success');
           formMessage.addClass(success ? 'success' : 'error');
@@ -31,7 +32,9 @@
         data: $form
       })
         .done(function (data) {
-          setMessage(parseInt(data) === 1);
+          var newsletter = typeof data === 'object',
+            condition = newsletter ? data.status === 'subscribed' : parseInt(data) === 1;
+          setMessage(condition, newsletter);
           form.reset();
         })
         .fail(function () {
