b52254db29f500ce2f18b80d512abba78dc8a720
Liliana Canpor <liliana.canpor@gmail.com>
2017-11-22 10:05:17 -0600

    #20 changes 21-11-17

--- src/js/cover.js
+++ src/js/cover.js
@@ -2,15 +2,30 @@
   var coverTitle = document.getElementsByClassName('homepage-hero__title');
   var coverWrapper = document.getElementsByClassName('homepage-hero_wrapper');
   var video = document.getElementById('cover-video');
-
-  video.oncanplay= function () {
-    setTimeout(function(){
-      coverTitle[0].classList.add('title-translate');
-      coverWrapper[0].classList.add('wrapper-translate');
-      setTimeout(function(){
-        coverTitle[0].classList.remove('title-translate');
-        coverWrapper[0].classList.remove('wrapper-translate');
-      }, 21000);
-    }, 7500);
-  };
+  var play = document.getElementById('play-btn');
+  console.log('jojojojo', screen.width);
+    if(screen.width > 767){
+      console.log('entro', screen.width);
+      video.oncanplay = function () {
+        setTimeout(function(){
+          coverTitle[0].classList.add('title-translate');
+          coverWrapper[0].classList.add('wrapper-translate');
+          setTimeout(function(){
+            coverTitle[0].classList.remove('title-translate');
+            coverWrapper[0].classList.remove('wrapper-translate');
+          }, 21000);
+        }, 7500);
+      };
+    }else if(screen.width < 767){
+      console.log('mobil');
+      play.classList.remove('hide');
+      video.oncanplay = function () {
+        play.addEventListener('touchstart', function (e) {
+          console.log(e);
+          e.preventDefault();
+          video.play();
+          play.classList.add('hide');
+        }, {passive: false});
+      };
+    }
 })();
