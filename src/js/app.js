d7cafa8c4ea7892b00ba8f3e828dd01f41068090
Liliana Canpor <liliana.canpor@gmail.com>
2017-11-22 13:27:00 -0600

    #20 add menu.js to app.js

--- src/js/app.js
+++ src/js/app.js
@@ -46,4 +46,56 @@
         });
     }
   });
+
+  //MOBILE MENU
+    var mobile = window.matchMedia('(max-width: 810px)');
+
+    function detectTouch(e) {
+      if (!$(e.target).is('nav .menu-trigger') && !$(e.target).is('nav .menu-container') && !$(e.target).closest('nav .menu-container').length && $('nav .menu-container').hasClass('show-menu')) {
+        e.preventDefault();
+        $('nav .menu-container').removeClass('show-menu');
+        $('body').removeClass('noscroll');
+      }
+    }
+
+    function detectClick() {
+      if ($('nav .menu-container').hasClass('show-menu')) {
+        $('nav .menu-container').removeClass('show-menu');
+        $('body').removeClass('noscroll');
+      } else {
+        $('nav .menu-container').addClass('show-menu');
+        $('body').addClass('noscroll');
+      }
+    }
+
+    if (mobile.matches) {
+      $('body')[0].addEventListener('touchstart', detectTouch, {passive: false});
+      $('nav .menu-trigger').click(detectClick);
+    } else {
+      $('body')[0].removeEventListener('touchstart', detectTouch, {passive: false});
+      $('nav .menu-trigger').off('click');
+      $('body').removeClass('noscroll');
+    }
+
+    $(window).resize(function () {
+      $('body')[0].removeEventListener('touchmove', detectTouch, {passive: false});
+      $('nav .menu-trigger').off('click');
+      $('body').removeClass('noscroll');
+      $('nav .menu-container').removeClass('show-menu');
+
+      if (mobile.matches) {
+        $('body')[0].addEventListener('touchmove', detectTouch, {passive: false});
+        $('nav .menu-trigger').click(detectClick);
+      }
+    });
+
+    if (!$('nav').hasClass('sections-nav')) {
+      $(window).scroll(function () {
+        if ($(this).scrollTop() > 20) {
+          $('nav').addClass('fixed');
+        } else {
+          $('nav').removeClass('fixed');
+        }
+      });
+    }
 })();
