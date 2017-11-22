ab9493bd920ec93c5541debe6424c25edd50542b
Liliana Canpor <liliana.canpor@gmail.com>
2017-11-22 13:50:03 -0600

    #20 pull changes

--- src/js/cover.js
+++ src/js/cover.js
@@ -1,47 +1,48 @@
-(function(){
+(function () {
   var coverTitle = document.getElementsByClassName('homepage-hero__title');
   var coverWrapper = document.getElementsByClassName('homepage-hero_wrapper');
   var video = document.getElementById('video');
   var play = document.getElementById('play-btn');
   var playCover = document.getElementById('video-cover');
-    if(screen.width > 767){
-      video.oncanplay = function () {
-        setTimeout(function(){
-          coverTitle[0].classList.add('title-translate');
-          coverWrapper[0].classList.add('wrapper-translate');
-          setTimeout(function(){
-            coverTitle[0].classList.remove('title-translate');
-            coverWrapper[0].classList.remove('wrapper-translate');
-          }, 21000);
-        }, 7500);
-      };
-    }else if(screen.width < 767){
-      play.classList.remove('hide');
-      video.removeAttribute('autoplay');
-      video.removeAttribute('loop');
-      video.oncanplay = function () {
-        play.addEventListener('touchstart', function () {
-            video.play();
-            play.classList.add('hide');
-        });
-        playCover.addEventListener('touchstart', function () {
-          video.play();
-          play.classList.add('hide');
-        });
-        play.addEventListener('click', function () {
-          video.play();
-          play.classList.add('hide');
-        });
-        playCover.addEventListener('click', function () {
-          video.play();
-          play.classList.add('hide');
-        });
-      };
-      video.onended = function () {
-        play.classList.remove('hide');
-      };
-      video.paused = function () {
-        play.classList.remove('hide');
-      };
+  if (screen.width > 767) {
+    video.oncanplay = function () {
+      setTimeout(function () {
+        coverTitle[0].classList.add('title-translate');
+        coverWrapper[0].classList.add('wrapper-translate');
+        setTimeout(function () {
+          coverTitle[0].classList.remove('title-translate');
+          coverWrapper[0].classList.remove('wrapper-translate');
+        }, 21000);
+      }, 7500);
     };
+  } else if (screen.width < 767) {
+    play.classList.remove('hide');
+    video.removeAttribute('autoplay');
+    video.removeAttribute('loop');
+    video.oncanplay = function () {
+      play.addEventListener('touchstart', function () {
+        video.play();
+        play.classList.add('hide');
+      });
+      playCover.addEventListener('touchstart', function () {
+        video.play();
+        play.classList.add('hide');
+      });
+      play.addEventListener('click', function () {
+        video.play();
+        play.classList.add('hide');
+      });
+      playCover.addEventListener('click', function () {
+        video.play();
+        play.classList.add('hide');
+      });
+    };
+    video.onended = function () {
+      play.classList.remove('hide');
+    };
+    video.paused = function () {
+      play.classList.remove('hide');
+    };
+  }
+  ;
 })();
