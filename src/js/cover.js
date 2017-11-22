5892d089a08ebc2ee889d0a7c5ef2739ed8b2bdd
Liliana Canpor <liliana.canpor@gmail.com>
2017-11-22 13:27:18 -0600

    #20 fixed vide playing on mbile

--- src/js/cover.js
+++ src/js/cover.js
@@ -1,11 +1,10 @@
 (function(){
   var coverTitle = document.getElementsByClassName('homepage-hero__title');
   var coverWrapper = document.getElementsByClassName('homepage-hero_wrapper');
-  var video = document.getElementById('cover-video');
+  var video = document.getElementById('video');
   var play = document.getElementById('play-btn');
-  console.log('jojojojo', screen.width);
+  var playCover = document.getElementById('video-cover');
     if(screen.width > 767){
-      console.log('entro', screen.width);
       video.oncanplay = function () {
         setTimeout(function(){
           coverTitle[0].classList.add('title-translate');
@@ -17,15 +16,32 @@
         }, 7500);
       };
     }else if(screen.width < 767){
-      console.log('mobil');
       play.classList.remove('hide');
+      video.removeAttribute('autoplay');
+      video.removeAttribute('loop');
       video.oncanplay = function () {
-        play.addEventListener('touchstart', function (e) {
-          console.log(e);
-          e.preventDefault();
+        play.addEventListener('touchstart', function () {
+            video.play();
+            play.classList.add('hide');
+        });
+        playCover.addEventListener('touchstart', function () {
           video.play();
           play.classList.add('hide');
-        }, {passive: false});
+        });
+        play.addEventListener('click', function () {
+          video.play();
+          play.classList.add('hide');
+        });
+        playCover.addEventListener('click', function () {
+          video.play();
+          play.classList.add('hide');
+        });
       };
-    }
+      video.onended = function () {
+        play.classList.remove('hide');
+      };
+      video.paused = function () {
+        play.classList.remove('hide');
+      };
+    };
 })();
